6528803acdc9f72f16763543383b57bda0d9d18b
kostas86 <serebrennikovnumberone@gmail.com>
2020-05-25 21:49:36 +0300

    25.05 start

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    let curveUrl = $("[data-js-curve]").attr("data-js-curve");
+    let valueBar = $(".curve-content-vertical");
+    let dataBar = $(".curve-content-horizontal");
+    let titleBar = $(".curve-content-top");
+    let curveWrap = $(".curve-content-wrap");
+
+
+    $.getJSON(curveUrl, function(data) {
+        initStart(data);
+    });
+    function initStart(content){
+        let curveName = content.name;
+        let curveNameContent = '<p>' + curveName + '</p>';
+        titleBar.append(curveNameContent);
+        fTest(curveName);
+        console.log(content);
+    }
+    function fTest(item){
+        console.log(item);
+    }
+
+});
